4368d7adcb4819b11f1e92fe8e6af6922872966a
Ahmed Faraz <ahmedcodes8y9@gmail.com>
2025-12-25 05:14:09 +0500

    Update docusaurus.config.js

--- book_robotic_minds/docusaurus.config.js
+++ book_robotic_minds/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://hackathon-book-1-nine.vercel.app/docs/intro',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
